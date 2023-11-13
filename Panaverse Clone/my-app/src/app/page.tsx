115d6f0d2deb955848d4c407b6db94b06cc1b000
talhak911 <talhakhan8300@gmail.com>
2023-11-13 21:35:22 +0500

    Next app

---
+++ Panaverse Clone/my-app/src/app/page.tsx
@@ -0,0 +1,19 @@
+import { Html } from 'next/document'
+import Image from 'next/image'
+
+export default function Home() {
+  return (    
+<Html>
+  <Headers>
+    <title>
+      mypage
+    </title>
+  </Headers>
+  <body>
+    <h1>
+      welcome
+    </h1>
+  </body>
+</Html>
+  )
+}
