58f3fdab8e7e2ffbae8f6b102f185f9f4f14f96b
talhak911 <talhakhan8300@gmail.com>
2023-11-14 20:32:39 +0500

    clone buliding start

--- Panaverse Clone/my-app/src/app/page.tsx
+++ Panaverse Clone/my-app/src/app/page.tsx
@@ -1,19 +1,27 @@
 import { Html } from 'next/document'
-import Image from 'next/image'
-
+import Image from 'next/image';
 export default function Home() {
-  return (    
-<Html>
-  <Headers>
-    <title>
-      mypage
-    </title>
-  </Headers>
-  <body>
-    <h1>
-      welcome
-    </h1>
-  </body>
-</Html>
-  )
+  const boxStyle = {
+    background: 'linear-gradient(120deg, #155799, #159957)', // Adjust gradient colors
+    color: 'white',
+    padding: '80px',
+    borderRadius: '10px',
+    
+    
+  };
+  return (
+    <body>
+      
+   
+    <div style={boxStyle}>
+      <h1 style={{textAlign: 'center'}}>Certified Web 3.0 and Metaverse Developer: A Nationwide Program in Karachi, Lahore, Islamabad, and Peshawar</h1>
+      <h3 style={{color: '#ffff',textAlign:'center'}}>Getting Ready for the Next Generation and Future of the Internet - Join a 13 Trillion Dollar Industry with 5 Billion Users</h3>
+      </div>
+      <h1  style={{color: '#32DB84',
+      textAlign:'center',
+      
+    }}>  Certified Web 3.0 and Metaverse Developer: A Nationwide Program in Karachi, Lahore, Islamabad, and Peshawar</h1>
+      </body>
+  );
 }
+
